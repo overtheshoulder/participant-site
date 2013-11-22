5e4affd323a2a429f264276a2d212f122878215b
Brad Sawicki <git@bradsawicki.com>
2013-11-22 14:38:16 -0600

    Home RWD

--- wp-content/themes/ots/page-2.php
+++ wp-content/themes/ots/page-2.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section id="hero" class="hero">
+<section id="hero" class="hero cf">
 	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<section class="content">
